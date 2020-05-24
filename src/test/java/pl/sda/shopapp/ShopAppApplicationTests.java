5e596110ea26349e706e45757c8b3bb3bf74c406
Kamil Jasek <Kamil.Jasek@dbschenker.com>
2020-05-24 11:38:27 +0200

    init commit

---
+++ src/test/java/pl/sda/shopapp/ShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.shopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
