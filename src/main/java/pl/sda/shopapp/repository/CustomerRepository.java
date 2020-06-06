9e99d2a0a5fc344d0315727d5f6c2d6620f020c0
Kamil Jasek <Kamil.Jasek@dbschenker.com>
2020-06-06 13:14:54 +0200

    repository query

--- src/main/java/pl/sda/shopapp/repository/CustomerRepository.java
+++ src/main/java/pl/sda/shopapp/repository/CustomerRepository.java
@@ -1,10 +1,14 @@
 package pl.sda.shopapp.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.sda.shopapp.entity.Customer;
 import pl.sda.shopapp.entity.Person;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 /**
@@ -20,4 +24,13 @@ public interface CustomerRepository extends JpaRepository<Customer, UUID> {
 
     // select * from Customer c where c.name like = ?1%
     List<Customer> findByNameStartingWith(String name);
+
+    @Query("from Customer c where c.taxId = ?1")
+    Optional<Customer> findByTaxId(String taxId);
+
+    @Query("from Person p where p.firstName like ?1 and p.lastName like ?2")
+    List<Person> findPerson(String firstName, String lastName);
+
+    @Query("select c from Customer c join c.addresses a where a.city = ?1")
+    Page<Customer> findByCity(String city, Pageable pageable);
 }
--- src/test/java/pl/sda/shopapp/CustomerRepositoryTest.java
+++ src/test/java/pl/sda/shopapp/CustomerRepositoryTest.java
@@ -1,18 +1,18 @@
 package pl.sda.shopapp;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import pl.sda.shopapp.entity.*;
 import pl.sda.shopapp.repository.CustomerRepository;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -57,7 +57,7 @@ final class CustomerRepositoryTest {
         company.addAddress(companyAddress);
 
         // when
-        repository.saveAll(Arrays.asList(person, company));
+        repository.saveAll(asList(person, company));
         repository.flush();
 //        repository.save(person);
 //        repository.save(company);
@@ -94,12 +94,63 @@ final class CustomerRepositoryTest {
         var company1 = new Company(new VatNumber("0123456789"), "Test S.A.");
         var company2 = new Company(new VatNumber("0123456789"), "Testuj S.A.");
         var person = new Person("Jan", "Kowalski", "89302002003");
-        repository.saveAll(Arrays.asList(company1, company2, person));
+        repository.saveAll(asList(company1, company2, person));
 
         // when
         var customers = repository.findByNameStartingWith("Test");
 
         // then
         assertEquals(2, customers.size());
+    }
+
+    @Test
+    void testHqlFindByTaxId() {
+        // given
+        var company1 = new Company(new VatNumber("0123456789"), "Test S.A.");
+        var company2 = new Company(new VatNumber("0123456781"), "Testuj S.A.");
+        repository.saveAll(asList(company1, company2));
+
+        // when
+        var customerOptional = repository.findByTaxId("0123456781");
+
+        // then
+        assertTrue(customerOptional.isPresent());
+        assertEquals(company2, customerOptional.get());
+    }
+
+    @Test
+    void testHqlFindByFirstNameAndLastName() {
+        // given
+        var person1 = new Person("Jan", "Kowalski", "89302002003");
+        var person2 = new Person("Jan", "Nowak", "89112002003");
+        repository.saveAll(asList(person1, person2));
+
+        // when
+        var customers = repository.findPerson("Jan%", "Kow%");
+
+        // then
+        assertEquals(1, customers.size());
+        assertEquals(person1, customers.get(0));
+    }
+
+    @Test
+    void testHqlFindCustomerByCity() {
+        // given
+        var company = new Company(new VatNumber("0123456789"), "Test S.A.");
+        var companyAddress = new Address("test", "Wawa", "01-500", "PL");
+        company.addAddress(companyAddress);
+
+        var person = new Person("Jan", "Kowalski", "89302002003");
+        var address = new Address("test", "Krakow", "01-500", "PL");
+        person.addAddress(address);
+
+        repository.saveAll(asList(company, person));
+
+        // when
+        var customers = repository.findByCity("Wawa", PageRequest.of(0, 10, Sort.by(Sort.Direction.ASC, "name")));
+
+        // then
+        assertEquals(1, customers.getTotalElements());
+        assertEquals(company, customers.getContent().get(0));
     }
 }
