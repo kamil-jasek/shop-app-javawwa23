b9ab434a9a6b445457010ea6b1d91a8818fee835
Kamil Jasek <Kamil.Jasek@dbschenker.com>
2020-06-20 14:02:51 +0200

    test controller

---
+++ src/test/java/pl/sda/shopapp/rest/CustomerRestControllerTest.java
@@ -0,0 +1,39 @@
+package pl.sda.shopapp.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * ... comment class...
+ *
+ * @author kamil.jasek@gmail.com
+ * @since 2020-06-20
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+class CustomerRestControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void testCreateCompany() throws Exception {
+        mvc.perform(post("/api/customers")
+                .header("Content-Type", "application/json")
+                .content("{\n" +
+                    "    \"name\": \"TEST 2 S.A.\",\n" +
+                    "    \"vatNumber\": \"0123456789\"\n" +
+                    "}"))
+                .andExpect(status().isCreated());
+    }
+}
