2942bf6e6edb0159d01fb95e32a6c30b49f5cce0
Kamil Jasek <Kamil.Jasek@dbschenker.com>
2020-06-21 12:40:16 +0200

    security

--- src/main/java/pl/sda/shopapp/ShopAppApplication.java
+++ src/main/java/pl/sda/shopapp/ShopAppApplication.java
@@ -2,8 +2,10 @@ package pl.sda.shopapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class ShopAppApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/pl/sda/shopapp/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package pl.sda.shopapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+/**
+ * ... comment class...
+ *
+ * @author kamil.jasek@gmail.com
+ * @since 2020-06-21
+ */
+@Configuration
+class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final DataSource dataSource;
+
+    SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers("/api/**").authenticated()
+            .and()
+                .csrf().disable()
+                .httpBasic()
+            .and()
+                .headers().frameOptions().sameOrigin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
